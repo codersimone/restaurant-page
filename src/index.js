c62696e3a4726f83f12a1235bb79e5f5e03867fd
codersimone <97359905+codersimone@users.noreply.github.com>
2025-09-05 11:23:57 +0300

    feat: implemented the transition and generation of the menu page module menu.js using JS

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import './styles/reset/reset.css';
 import './styles/styles.css';
 import loadHome from './modules/pages/home.js';
+import loadMenu from './modules/pages/menu.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     loadHome();
 });
 
-document.querySelector('homeBtn').addEventListener('click', loadHome);
+document.querySelector('#homeBtn').addEventListener('click', loadHome);
+document.querySelector('#menuBtn').addEventListener('click', loadMenu);
---
+++ src/modules/pages/menu.js
@@ -0,0 +1,20 @@
+export default function loadMenu() {
+    const content = document.querySelector('#content');
+    content.textContent = '';
+
+    const heading = document.createElement('h1');
+    heading.classList.add('menuHeading');
+    heading.textContent = 'Our menu';
+
+    const list = document.createElement('ul');
+    const items = ['Appetizer', 'Salad', 'Main dish', 'Side dish', 'Dessert'];
+
+    items.forEach((item) => {
+        const itemsList = document.createElement('li');
+        itemsList.textContent = item;
+        list.appendChild(itemsList);
+    });
+
+    content.appendChild(heading);
+    content.appendChild(list);
+}
