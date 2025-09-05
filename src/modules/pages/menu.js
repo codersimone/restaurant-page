d783cd39c584f381922b2b34f375e425979cbe1a
codersimone <97359905+codersimone@users.noreply.github.com>
2025-09-05 13:26:36 +0300

    feat: added a message for menu items that are under development

--- src/modules/pages/menu.js
+++ src/modules/pages/menu.js
@@ -8,11 +8,29 @@ export default function loadMenu() {
 
     const list = document.createElement('ul');
     list.classList.add('menuList');
+
     const items = ['Appetizer', 'Salad', 'Main dish', 'Side dish', 'Dessert'];
 
     items.forEach((item) => {
         const itemsList = document.createElement('li');
+        itemsList.classList.add('menuDisabledItem');
         itemsList.textContent = item;
+
+        const message = document.createElement('span');
+        message.classList.add('menuPlug');
+        message.textContent = ` - this section will be ready soon`;
+
+        itemsList.appendChild(message);
+
+        itemsList.addEventListener('click', (e) => {
+            e.preventDefault();
+
+            message.classList.add('show');
+            setTimeout(() => {
+                message.classList.remove('show');
+            }, 2000);
+        });
+
         list.appendChild(itemsList);
     });
 
