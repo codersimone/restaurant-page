5ee6ae44679f8e43e962b2243dd7f41c7010e1cd
codersimone <97359905+codersimone@users.noreply.github.com>
2025-09-28 23:44:01 +0400

    feat: added menu accordion initialization when clicking on the Menu button

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import './styles/styles.css';
 import loadHome from './modules/pages/home.js';
 import loadMenu from './modules/pages/menu.js';
 import loadContact from './modules/pages/contact.js';
+import { initMenuControls } from './modules/controls/menuControls.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     loadHome();
     document.querySelector('#homeBtn').addEventListener('click', loadHome);
-    document.querySelector('#menuBtn').addEventListener('click', loadMenu);
+    document.querySelector('#menuBtn').addEventListener('click', () => {
+        const menuList = loadMenu();
+        initMenuControls(menuList);
+    });
     document
         .querySelector('#contactBtn')
         .addEventListener('click', loadContact);
